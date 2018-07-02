af09b64e87089efed6607aa9f622552432901dd0
董淑伟 <dsw0602@foxmail.com>
2018-07-02 16:51:40 +0800

    add routes and run the projects successfully

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/')));
+
+
+app.get('/', function(req, res) {
+    res.sendFile('/Users/hammer/Downloads/itering/code/js-eth-personal-sign-examples/index.html');
+});
+
+app.listen(8090, function(){console.log('Listening on port 8090...');});
