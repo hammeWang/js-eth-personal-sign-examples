03aaf03b4fa3d42d9b68f960bb7912f19a6fb010
董淑伟 <dsw0602@foxmail.com>
2018-07-02 18:48:53 +0800

    modify the path of index.html in app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, '/')));
 
 
+
+
 app.get('/', function(req, res) {
-    res.sendFile('/Users/hammer/Downloads/itering/code/js-eth-personal-sign-examples/index.html');
+    res.sendFile(path.join(__dirname, '/') + 'index.html');
 });
 
 app.listen(8080, function(){console.log('Listening on port 8080...');});
